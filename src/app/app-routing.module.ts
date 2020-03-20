5bd43b89aa21f52c6c62f223cf4e2f81878816f8
Raniela <daniela.paz.me@gmail.com>
2020-03-19 21:29:04 -0300

    Despliegue Angular a Heroku

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+
+
+const app_routes: Routes = [
+    {path: '', component: PortafolioComponent },
+    {path: '', component: PortafolioComponent }
+];
+
+@NgModule({
+
+})  
+
+export class AppRoutingModule {
+
+}
+
